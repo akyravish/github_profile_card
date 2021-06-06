7374265c7473affc5e74f73aadc5fba3e1cad87c
akyravish <info.akshayravish@gmail.com>
2021-06-06 14:45:18 +0530

    add two function to search the value and get the api

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const APIURL = 'https://api.github.com/users/';
 
+const form = document.getElementById('form');
+const searchData = document.querySelector('.search');
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	let searchValue = searchData.value;
+	getUsers(searchValue);
+	form.reset();
+});
+
+async function getUsers(username) {
+	const { data } = await axios(APIURL + username);
+	console.log(data);
+}
