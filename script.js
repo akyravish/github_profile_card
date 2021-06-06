b8979415f3cde129a271c130ca88b4422cc01a71
akyravish <info.akshayravish@gmail.com>
2021-06-06 15:24:19 +0530

    add lines to change the avatar adn name

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const APIURL = 'https://api.github.com/users/';
 
 const form = document.getElementById('form');
 const searchData = document.querySelector('.search');
+const avatar = document.querySelector('.avatar');
+const main = document.getElementById('main');
 
+// get search value
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 	let searchValue = searchData.value;
@@ -10,7 +13,40 @@ form.addEventListener('submit', (e) => {
 	form.reset();
 });
 
+// get api request
 async function getUsers(username) {
-	const { data } = await axios(APIURL + username);
-	console.log(data);
+	try {
+		const { data } = await axios(APIURL + username);
+		const reposUrl = `${APIURL + username}/repos`;
+		console.log(reposUrl);
+		main.innerHTML = `
+             <div class="card">
+                <div>
+                    <img src=${data.avatar_url} alt="avatar" class="avatar">
+                </div>
+                <div class="user-info">
+                    <h2>${data.name}</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, earum!</p>
+                    <ul>
+                    <li>${data.followers}<strong> Followers</strong></li>
+                    <li>${data.following}<strong> Following</strong></li>
+                    <li>${data.public_repos}<strong> Repos</strong></li>
+                    </ul>
+                    <div id="repos">
+                    <a href="#" class="repo">Repos 1</a>
+                    <a href="#" class="repo">Repos 2</a>
+                    <a href="#" class="repo">Repos 3</a>
+                    <a href="#" class="repo">Repos 3</a>
+                    <a href="#" class="repo">Repos 3</a>
+                    <a href="#" class="repo">Repos 3</a>
+                    <a href="#" class="repo">Repos 3</a>
+                    </div>
+                </div>
+            </div>
+        `;
+
+		console.log(data);
+	} catch (error) {
+		console.log(error);
+	}
 }
