73f64d172735ac84ca851f24a9f290ca2527ed83
akyravish <info.akshayravish@gmail.com>
2021-06-06 20:28:51 +0530

    add all the necessary code for the project

--- script.js
+++ script.js
@@ -11,12 +11,50 @@ form.addEventListener('submit', (e) => {
 	let searchValue = searchData.value;
 	if (searchValue) {
 		getUsers(searchValue);
-		searchValue = '';
 	}
-
 	form.reset();
 });
 
+// getProfile function
+const getProfile = (user) => {
+	main.innerHTML = `
+             <div class="card">
+                <div>
+                    <img src=${user.avatar_url} alt="avatar" class="avatar">
+                </div>
+                <div class="user-info">
+                    <h2>${user.name}</h2>
+                    <p>${
+						user.bio === null
+							? 'There is nothing to show.'
+							: user.bio
+					}</p>
+                    <ul>
+                    <li>${user.followers}<strong> Followers</strong></li>
+                    <li>${user.following}<strong> Following</strong></li>
+                    <li>${user.public_repos}<strong> Repos</strong></li>
+                    </ul>
+                    <div id="repos">
+                    </div>
+                </div>
+            </div>
+        `;
+};
+
+// getRepo function
+const getRepo = (repos) => {
+	repos.splice(0, 5).forEach((repo) => {
+		const reposEl = document.getElementById('repos');
+		const linkEl = document.createElement('a');
+		linkEl.classList.add('repo');
+		linkEl.href = repo.html_url;
+		linkEl.innerText = repo.name;
+		linkEl.target = '_blank';
+		reposEl.appendChild(linkEl);
+	});
+};
+
+// error card function
 const errorCard = () => {
 	main.innerHTML = `<div class="card error">Sorry, This user doesn't exit.</div>`;
 };
@@ -25,32 +63,12 @@ const errorCard = () => {
 async function getUsers(username) {
 	try {
 		const { data } = await axios(APIURL + username);
-		const reposUrl = `${APIURL + username}/repos`;
-		console.log(reposUrl);
-		main.innerHTML = `
-             <div class="card">
-                <div>
-                    <img src=${data.avatar_url} alt="avatar" class="avatar">
-                </div>
-                <div class="user-info">
-                    <h2>${data.name}</h2>
-                    <p>${
-						data.bio === null
-							? 'There is nothing to show.'
-							: data.bio
-					}</p>
-                    <ul>
-                    <li>${data.followers}<strong> Followers</strong></li>
-                    <li>${data.following}<strong> Following</strong></li>
-                    <li>${data.public_repos}<strong> Repos</strong></li>
-                    </ul>
-                    <div id="repos">
-                    </div>
-                </div>
-            </div>
-        `;
+		const reposUrl = await axios(
+			APIURL + username + '/repos?sort="created"'
+		);
 
-		console.log(data);
+		getProfile(data);
+		getRepo(reposUrl.data);
 	} catch (error) {
 		if (error.response.status === 404) {
 			errorCard();
